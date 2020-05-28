71d31c39941905f14f89bd491539b8e99bd8dd33
jrmywre <jrmywre@gmail.com>
2020-05-28 02:36:05 -0400

    test

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -9,7 +9,7 @@
 $dbServername = "us-cdbr-east-05.cleardb.net";
 $dbUsername = "b80fee89eaf263";
 $dbPassword = "48b36ca6";
-$dbName = "heroku_e3fa0ff8311b9cc";
+$dbName = "heroku_e3fa0ff8311b9cc/article";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
