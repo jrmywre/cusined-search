f481498222b61f97a99acfa496bf89633eddcf10
jrmywre <jrmywre@gmail.com>
2020-05-29 01:36:57 -0400

    testing

--- includes/dbh-inc.php
+++ includes/dbh-inc.php
@@ -1,15 +1,15 @@
 <?php 
 
 
-// $dbServername = "localhost";
-// $dbUsername = "root";
-// $dbPassword = "";
-// $dbName = "dbsearchsystem";
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "dbsearchsystem";
 
-$dbServername = "us-cdbr-east-05.cleardb.net";
-$dbUsername = "b80fee89eaf263";
-$dbPassword = "48b36ca6";
-$dbName = "heroku_e3fa0ff8311b9cc/article";
+// $dbServername = "us-cdbr-east-05.cleardb.net";
+// $dbUsername = "b80fee89eaf263";
+// $dbPassword = "48b36ca6";
+// $dbName = "heroku_e3fa0ff8311b9cc/article";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
 
